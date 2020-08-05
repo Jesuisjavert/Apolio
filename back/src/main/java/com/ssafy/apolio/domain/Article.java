9d929231e4e626339afcc281a6d6fd1a307297e1
ParkSeungBeom <psb9559@naver.com>
2020-08-05 16:27:24 +0900

    apolio back3) article id => auto increment로 수정

--- back/src/main/java/com/ssafy/apolio/domain/Article.java
+++ back/src/main/java/com/ssafy/apolio/domain/Article.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class Article {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "article_id")
     private Long id;
 
