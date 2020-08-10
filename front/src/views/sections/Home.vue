d4459d34aed5b32ee715173abd610853c9c93a73
강동훈 <jesuisjavert@gmail.com>
2020-08-10 22:17:51 +0900

    Apolio front1) Axios Crawling 작업중

--- front/src/views/sections/Home.vue
+++ front/src/views/sections/Home.vue
@@ -6,39 +6,16 @@
     tag="section"
   >
     <home-headline />
-    <div>
-      {{crawling.title}}
-    </div>
+
   </v-container>
 </template>
 
 <script>
-  import axios from 'axios'
-
-  const API_URL = 'http://127.0.0.1:8080'
-
   export default {
     name: 'Home',
 
     components: {
       HomeHeadline: () => import('@/components/base/HomeHeadline'),
-    },
-
-    data () {
-      return {
-        crawling: [],
-      }
-    },
-    created () {
-      this.loadCrawling()
-    },
-    methods: {
-      loadCrawling () {
-            axios.get(`${API_URL}/api/`).then((res) => {
-          this.crawling = res.data
-        })
-        console.log(this.crawling)
-      },
     },
   }
 </script>
